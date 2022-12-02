070af63da4decb0940a33a915fe61516d8585b3e
soykot360 <39725418+soykot360@users.noreply.github.com>
2022-12-02 17:14:20 +0600

    Changes

---
+++ plugins/diskspace/lang/it.js
@@ -0,0 +1,11 @@
+﻿/*
+ * PLUGIN DISKSPACE
+ *
+ * Italian language file.
+ *
+ * Author: 
+ */
+
+ theUILang.diskNotification	= "Attenzione! Il disco è pieno. rTorrent potrebbe non funzionare correttamente, nessun dato verrà scaricato fino a che non liberi spazio su disco.";
+
+thePlugins.get("diskspace").langLoaded();
